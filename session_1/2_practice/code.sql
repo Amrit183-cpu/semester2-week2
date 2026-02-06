98837aa61d15b110bdc8734624c285d24eb0ba47
Amrit183-cpu <rhrt0563@leeds.ac.uk>
2026-02-06 11:38:47 +0000

    2.2 2_practice

--- session_1/2_practice/code.sql
+++ session_1/2_practice/code.sql
@@ -17,9 +17,12 @@
 --6 select name, count(Loans.id) as num_of_loans from Members join Loans on Members.id = Loans.member_id group by name;
 --7 select name, count(Loans.id) as num_of_loans from Members Left join Loans on Members.id = Loans.member_id group by name having num_of_loans=0;
 --8 select LibraryBranch.name, Books.title, count(Loans.id) from Books join LibraryBranch on Books.branch_id = LibraryBranch.id, Loans on Books.id = Loans.book_id group by Books.title;
---9
-select Members.name, Loans.loan_date
-from Members left join Loans on Members.id = Loans.member_id
-group by Members.name 
-having Loans.return_date > date('now')
--- not sure about 9
+--9 select Members.name, Loans.loan_date from Members left join Loans on Members.id = Loans.member_id group by Members.name having Loans.return_date > date('now');
+--10
+select Books.title, Loans.id, count(Loans.id) as num_of_loans,
+case --do case statement
+when count(Loans.id) = 0 then 'unloaned book' --if not loaned
+else 'loaned book' 
+end as loan_status --column for loan status
+from Books left join Loans on Books.id = Loans.book_id
+group by Books.title;
