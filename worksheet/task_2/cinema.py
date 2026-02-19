1ee208191af0b70557e1ab015ea36741b327a470
Amrit183-cpu <rhrt0563@leeds.ac.uk>
2026-02-19 18:40:15 +0000

    worksheet 2.2

--- worksheet/task_2/cinema.py
+++ worksheet/task_2/cinema.py
@@ -1,3 +1,5 @@
+#Amritpal Singh
+#201965262
 """
 This is where you should write your code and this is what you need to upload to Gradescope for autograding.
 
@@ -18,8 +20,23 @@ def customer_tickets(conn, customer_id):
     Include only tickets purchased by the given customer_id.
     Order results by film title alphabetically.
     """
-    pass
+    conn = sqlite3.connect("tickets.db") 
+    cursor = conn.cursor()
 
+    cursor.execute('''select films.title, screenings.screen, tickets.price
+                   from screenings join tickets on screenings.screening_id = tickets.screening_id,
+                    films on films.film_id = screenings.film_id
+                   where tickets.customer_id ?  (customer_id,) 
+                   order by films.title asc''') 
+    
+   
+    
+    #the query above selected the film title, screen and prices of the tickets
+    #joined the tables screenings to films and tickets (as it is the middle table)
+    #ordered in alphabetical from a-z
+    
+    y = cursor.fetchall() #fetch all rows
+    return y
 
 def screening_sales(conn):
     """
@@ -29,7 +46,21 @@ def screening_sales(conn):
     Include all screenings, even if tickets_sold is 0.
     Order results by tickets_sold descending.
     """
-    pass
+    
+    conn = sqlite3.connect("tickets.db")
+    cursor = conn.cursor()
+
+    cursor.execute('''select screenings.screening_id, films.title, count(tickets.ticket_id) as num_of_tickets
+                   from screenings left join films on screenings.film_id = films.film_id, tickets on screenings.screening_id = tickets.screening_id
+                   group by screenings.screening_id
+                   order by num_of_tickets desc''')
+
+    #query above selects the screening_id, film title and counts tickets sold for that screen
+    #do left join to include 0 sales, joins screenings to films and tickets (since it is middle table)
+    #order by ticket sales from highest to lowest
+
+    x = cursor.fetchall() #fetch all results
+    return x
 
 
 def top_customers_by_spend(conn, limit):
@@ -42,4 +73,23 @@ def top_customers_by_spend(conn, limit):
     Order by total_spent descending.
     Limit the number of rows returned to `limit`.
     """
-    pass
+    
+    conn = sqlite3.connect("tickets.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""select customers.customer_name, sum(tickets.price) 
+                   from customers join tickets on customers.customer_id = tickets.customer_id
+                   group by customers.customer_name
+                   order by sum(tickets.price) desc
+                   limit ?""", (limit,) ) 
+                   
+    #query above adds all ticket prices for each customer individually
+    #join customers to tickets
+    #order by total spent by customer on tickets from highest to lowest
+    #set a limit by passsing limit parameter as a tuple 
+    #code didn't allow me to rename aggregate to 'total_spent'
+    #so not sure if i implemented the num_of_tickets > 0, did it by only join and not left join
+
+    z = cursor.fetchall() #fetch all results
+    return z
+    
